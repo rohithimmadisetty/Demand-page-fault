f70a79aa1de947e1b1d2621fb299e84c8a80dc9f
rohithimmadisetty <43609611+rohithimmadisetty@users.noreply.github.com>
2019-04-25 12:14:20 +0530

    Add files via upload

---
+++ os2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	float pagefault_empty=8*pow(10,-3);
+	float pagefault_modified=20*pow(10,-3);
+	float MAT=100*pow(10,-9);
+	float modified_time=0.70;
+	float MApagefault=200*pow(10,-9);
+	float pagefault_rate;
+	/*formula for efficient acceptable time is  
+        EAT<=(1-p)Memory_access_time + p(page_fault_service time)
+        page fault service time=(replaced modified time*time to service a page fault when page is replaced)+
+		                        ((1-replaced modified time)*time to service a page fault whenpage is empty)*/
+	float p=modified_time*pagefault_modified+(1-modified_time)*pagefault_empty;
+	pagefault_rate=(MApagefault-MAT)/(p-MAT);
+	cout<<" Maximum accaepatble page fault_rate rate is "<<pagefault_rate;
+	
+}
